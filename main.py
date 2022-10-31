6ad2aba73de8d84bd57e85ef163ede187d206c1a
BBoltzmann <woleoshoifeoluwa@gmail.com>
2022-10-31 20:25:05 +0100

    test bool

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+from crypt import methods
+from tkinter.font import BOLD
 from flask import *
 import json, time
 
@@ -6,11 +9,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def  home_page():
-    a = "hey"
-    # data_set={"slackUsername":'Bboltzmann', "backend": True, "age": 99, "bio": 'Hate me' }
-    # json_dump = json.dumps(data_set)
+    data_set={"slackUsername":'Bboltzmann', "backend": True, "age": 99, "bio": 'Hate me' }
+    json_dump = json.dumps(data_set)
 
-    return a
+    return json_dump
 
 # @app.route('/user/', methods=['GET'])
 # def  request_page():
