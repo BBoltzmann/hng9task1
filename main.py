d055428d974d8d6e1eceae61d53d170e7e5cd582
BBoltzmann <woleoshoifeoluwa@gmail.com>
2022-10-31 19:37:25 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from crypt import methods
+from tkinter.font import BOLD
 from flask import *
 import json, time
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def  home_page():
-    data_set={"slackUsername":'Bboltzmann', "backend": 'true', "age": 99, "bio": 'Hate me' }
+    data_set={"slackUsername":'Bboltzmann', "backend": True, "age": 99, "bio": 'Hate me' }
     json_dump = json.dumps(data_set)
 
     return json_dump
